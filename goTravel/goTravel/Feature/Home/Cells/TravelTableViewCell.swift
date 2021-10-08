81c0b9c5226d2fb0f7e704b6b6fe8dcefe6cc425
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 <pedro.henrique.leda@gmail.com>
2021-10-07 23:27:13 -0300

    created custom cells

---
+++ goTravel/goTravel/Feature/Home/Cells/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  goTravel
+//
+//  Created by Pedro Henrique on 07/10/21.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
