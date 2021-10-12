e0dbaf700100085a8b6c9b6cf034e5a555b887f1
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 <pedro.henrique.leda@gmail.com>
2021-10-12 19:06:45 -0300

    config cells in tableview

--- goTravel/goTravel/Feature/Home/Cells/TravelTableViewCell.swift
+++ goTravel/goTravel/Feature/Home/Cells/TravelTableViewCell.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class TravelTableViewCell: UITableViewCell {
-
-  // MARK: - IBOutlets
+    
+    // MARK: - IBOutlets
     @IBOutlet weak var backgroundViewCell: UIView!
     @IBOutlet weak var viagemImage: UIImageView!
     @IBOutlet weak var tituloViagemLabel: UILabel!
@@ -18,4 +18,24 @@ class TravelTableViewCell: UITableViewCell {
     @IBOutlet weak var precoSemDescontoLabel: UILabel!    
     @IBOutlet weak var precoViagemLabel: UILabel!
     @IBOutlet weak var statusCancelamentoLabel: UILabel!
+    
+    // MARK: - Methods
+    func configCell (_ viagem: Viagem?){
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+        
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0)")
+        
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        
+        precoSemDescontoLabel.attributedText = atributoString
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numeroDeDias == 1 ? "Diária" : "Diarias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+    }
 }
